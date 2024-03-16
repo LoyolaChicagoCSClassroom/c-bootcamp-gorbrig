8a4f77fd2218ce9023c9f8e00b841d7abd29ac7b
Bridget Gorman <birdget22@gmail.com>
2024-03-16 01:11:36 -0500

    added the rest of arithmetic, stack manipulation, double element operations, and misc tests

--- forth/int_stack_tests.cc
+++ forth/int_stack_tests.cc
@@ -143,17 +143,187 @@ TEST(IntStackTests, Swap) {
 
 TEST(IntStackTests, Add) {
 	int_stack_t stack;
-	int_stack_init(&stacl, 3); //initalizes the capacity tp 3
+	int_stack_init(&stack, 3); //initalizes the capacity to 3
 	int_stack_push(&stack, 10);
 	int_stack_push(&stack, 20);
-	ASSERT_EQ(int_stack_add(&stack), 1); //addition should happen
-
+	int_stack_add(&stack);
+	
 	int top_value;
-	int_stack_top(&stack, &top_value);
+	ASSERT_TRUE(int_stack_top(&stack, &top_value)):
 	ASSERT_EQ(top_value, 30); // checks if the result is cirect 
 }
 
+TEST(IntStackTests, Subtraction){
+	int_stack_t stack; 
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 5);
+	int_stack_push(&stack, 3);
+	int_stack_subtraction(&stack);
 
+	int top_value;
+	ASSERT_TRUE(int_stack_top(&stack, &top_value));
+	ASSERT_EQ(top_value, 2);
+}
+
+TEST(IntStackTests, Multiply) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 2);
+	int_stack_push(&stack, 3);
+	int_stack_multiply(&stack);
+
+	int_top_value;
+	ASSERT_TRUE(int_stack_top(&stack, &top_value));
+	ASSERT_EQ(top_value, 6);
+}
+
+
+TEST(IntStackTests, Divide) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 6);
+	int_stack_push(&stack, 3);
+	int_stack_divide(&stack);
+	
+	int top_value;
+	ASSERT_TRUE(int_stack_top(&stack, &top_value));
+	ASSERT_EQ(top_value, 2); // Verify 6 / 3 = 2
+}
+
+TEST(IntStackTests, DivMod) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 10);
+	int_stack_push(&stack, 3);
+	int_stack_divmod(&stack);
+	
+	int remainder, quotient;
+   	ASSERT_TRUE(int_stack_pop(&stack, &quotient)); // Pop quotient first due to stack order
+	ASSERT_TRUE(int_stack_top(&stack, &remainder)); // Then check the remainder on top
+	ASSERT_EQ(quotient, 3); // Verify quotient of 10 / 3 = 3
+	ASSERT_EQ(remainder, 1); // Verify remainder of 10 % 3 = 1
+}
+
+TEST(IntStackTests, Mod) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 10);
+	int_stack_push(&stack, 3);
+	ASSERT_TRUE(int_stack_mod(&stack));
+
+	int result; 
+	ASSERT_TRUE(int_stack_top(&stack, &result);
+	ASSERT_EQ(result, 1);
+}
+
+//stack manipulation 
+TEST(intStackTests, Over) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 1);
+	int_stack_push(&stack, 2);
+	ASSERT_TRUE(int_stack_over(&stack));
+	
+	int top_value;
+	ASSERT_TRUE(int_stack_top(&stack, &top_value)); 
+	ASSERT_EQ(top_value, 1); // verify the second to top value 1 is now top
+}
+
+TEST(IntStackTests, Rot) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 1);
+	int_stack_push(&stack, 2);
+	int_stack_push(&stack, 3);
+	ASSERT_TRUE(int_stack_rot(&stack));
+
+	int third_value;
+	int_stack_pop(&stack, &third_value); // Remove what is now the top value to get to the rotated one
+	ASSERT_EQ(third_value, 1); // Verify the original bottom value (1) is now on top
+}
+
+TEST(IntStackTests, Drop) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 1);
+	ASSERT_TRUE(int_stack_drop(&stack)); // Perform drop operation
+	ASSERT_EQ(int_stack_size(&stack), 0); // Confirm the stack is empty after dropping the element
+}
+
+//double element operations 
+
+TEST(IntStackTests, 2Swap) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 1);
+	int_stack_push(&stack, 2);
+	int_stack_push(&stack, 3);
+	int_stack_push(&stack, 4);
+	ASSERT_TRUE(int_stack_2swap(&stack));
+	
+	int top_value, next_value;
+	int_stack_pop(&stack, &top_value); // Should now be 2, previously the second pair's top
+	int_stack_pop(&stack, &next_value); // Should now be 1, previously the second pair's bottom
+	ASSERT_EQ(top_value, 2);
+	ASSERT_EQ(next_value, 1); // Confirm the top two pairs have been swapped
+}
+
+TEST(IntStackTests, 2Dup) {
+	int_stack_t stack; 
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 1);
+	int_stack_push(&stack, 2);
+	ASSERT_TRUE(int_stack_2dup(&stack));
+	ASSERT_EQ(int_stack_size(&stack), 4); //stack size should now be 4 after duplication
+
+	int top_value;
+	int_stack_pop(&stack, &top_value);
+	ASSERT_EQ(top_value, 2); // the last duplicated value should be 2
+}
+
+TEST(IntStackTests, 2Over) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 1);
+	int_stack_push(&stack, 2);
+	int_stack_push(&stack, 3);
+	int_stack_push(&stack, 4);
+	ASSERT_TRUE(int_stack_2over(&stack));
+	
+	int top_value, next_value;
+	int_stack_pop(&stack, &top_value); // Should be 2, the over value of the second pair
+	int_stack_pop(&stack, &next_value); // Should be 1, the over value of the second
+}
+TEST(IntStackTests, 2Drop) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 1);
+	int_stack_push(&stack, 2);
+	ASSERT_TRUE(int_stack_2drop(&stack) == 0);//sucess on dropping 2 elements 
+	ASSERT_EQ(int_stack_size(&stack), 0);
+}
+
+
+
+TEST(IntStackTests, Print) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 1);
+}
+
+TEST(IntStackTests, Size) {
+	int_stack_t stack;
+	int_stack_init(&stack, 10);
+	int_stack_push(&stack, 1);
+	int_stack_push(&stack, 2);
+	ASSERT_EQ(int_stack_size(&stack), 2); // verify the size is 2 after 2 pushes 
+}
+
+TEST(IntStackTests, Capacity) {
+	int_stack_t stack; 
+	int_stack_init(&stack, 10);
+	ASSERT_EQ(int_stack_capacity(&stack), 10);
+}
 
 int main(int argc, char **argv)
 {
